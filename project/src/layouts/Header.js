4b1ecab6cb8334eb83628af1003048cad4c039d2
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-23 21:25:18 +0200

    Set the display of pictures

--- project/src/layouts/Header.js
+++ project/src/layouts/Header.js
@@ -1,16 +1,41 @@
 import React from "react";
 import "../styles/Header.css";
+import {Route, Switch} from "react-router-dom";
 
 import img1 from '../images/header1.jpg'
 import img2 from '../images/header2.jpg'
 import img3 from '../images/header3.jpg'
 import img4 from '../images/header4.jpg'
-import img5 from '../images/header5.jpg'
-
 
 const Header = () => {
+
+    // const images = [img1, img2, img3, img4];
+    // const index = Math.floor(Math.random() *4);
+    //
+    // const img = images[index];
+
     return (
-        <img src={img1} alt="city"/>
+        <>
+            <Switch>
+                <Route path="/products" render={() => (
+                    <img src={img1} alt="city"/>
+                )} />
+                <Route path="/contact" render={() => (
+                    <img src={img2} alt="city"/>
+                )} />
+                <Route path="/admin" render={() => (
+                    <img src={img3} alt="city"/>
+                )} />
+                <Route path="/" exact render={() => (
+                    <img src={img4} alt="city"/>
+                )} />
+                <Route render={() => (
+                    <img src={img4} alt="city"/>
+                )} />
+            </Switch>
+
+            {/*<img src={img} alt="city"/>*/}
+        </>
     );
 }
 
