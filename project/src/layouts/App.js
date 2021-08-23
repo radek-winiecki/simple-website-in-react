ab64fb1201ef6f28a026e5be9fccfc95f3ca0e88
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-23 13:15:41 +0200

    Start creating a new project

---
+++ project/src/layouts/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import '../styles/App.css';
+
+class App extends React.Component {
+  render () {
+    return (
+        <div className="App">
+
+        </div>
+    );
+  }
+}
+
+export default App;
