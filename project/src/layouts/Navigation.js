84b48958d2dae6bc6ea300b079bdf90ea5429c2a
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-23 13:59:28 +0200

    Create a Navigation and display it

--- project/src/layouts/Navigation.js
+++ project/src/layouts/Navigation.js
@@ -1,8 +1,29 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+
+const list = [
+    {name: "start", path: "/"},
+    {name: "produkty", path: "/products"},
+    {name: "kontakt", path: "/contact"},
+    {name: "panel admina", path: "/admin"},
+]
 
 const Navigation = () => {
+
+    const menu = list.map(item => (
+        <li key={item.name}>
+            <NavLink to={item.path}>
+                {item.name}
+            </NavLink>
+        </li>
+    ))
+
     return (
-        <div>Navigation</div>
+        <nav>
+            <ul>
+                {menu}
+            </ul>
+        </nav>
     );
 }
 
