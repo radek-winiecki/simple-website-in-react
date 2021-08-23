e1772766108331dacbf727f638430fd1503639ae
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-23 13:28:57 +0200

    Create an XML structure in the App.js file

--- project/src/layouts/App.js
+++ project/src/layouts/App.js
@@ -3,15 +3,28 @@ import '../styles/App.css';
 import {BrowserRouter as Router} from "react-router-dom";
 
 class App extends React.Component {
-  render () {
-    return (
-        <Router>
-            <div className="App">
-
-            </div>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <header>
+                        {<Header/>}
+                    </header>
+                    <main>
+                        <aside>
+                            {<Navigation/>}
+                        </aside>
+                        <section className="page">
+                            {<Page/>}
+                        </section>
+                    </main>
+                    <footer>
+                        {<Footer/>}
+                    </footer>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
