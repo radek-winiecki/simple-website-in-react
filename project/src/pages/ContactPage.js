b50794621f81148808ca2c993428ffee018d0b1c
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-24 00:43:06 +0200

    Create a contact section

--- project/src/pages/ContactPage.js
+++ project/src/pages/ContactPage.js
@@ -1,9 +1,41 @@
 import React from "react";
+import '../styles/ContactPage.css';
 
-const ContactPage = () => {
-    return (
-        <div>Kontakt</div>
-    );
+class ContactPage extends React.Component {
+
+    state = {
+        value: "",
+
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            value: "",
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        placeholder="Wpisz wiadomość...">
+                    </textarea>
+                    <button>Wyślij</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default ContactPage;
