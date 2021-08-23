d927f1d77069d3a9f181bf7f57cd134b1c4ab71b
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-23 21:51:21 +0200

    Add articles to the home page

--- project/src/pages/HomePage.js
+++ project/src/pages/HomePage.js
@@ -1,8 +1,37 @@
 import React from "react";
+import Article from "../components/Article";
+
+const articles = [
+    {
+        id: 1,
+        title: "Czym jest teoria strun?",
+        author: "Jan Nowak",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia neque at risus tempus, ac condimentum nulla facilisis. Nunc sed consectetur lorem. Vivamus condimentum at lacus et malesuada. Integer luctus lacus vitae quam varius, quis imperdiet neque cursus. Pellentesque eros dui, volutpat non commodo non, pulvinar ut arcu. Integer id dictum neque, nec ultrices lectus. Phasellus scelerisque ante ac metus fermentum egestas.",
+    },
+    {
+        id: 2,
+        title: "Czym jest paradoks Fermiego?",
+        author: "Jan Kowalski",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia neque at risus tempus, ac condimentum nulla facilisis. Nunc sed consectetur lorem. Vivamus condimentum at lacus et malesuada. Integer luctus lacus vitae quam varius, quis imperdiet neque cursus. Pellentesque eros dui, volutpat non commodo non, pulvinar ut arcu. Integer id dictum neque, nec ultrices lectus. Phasellus scelerisque ante ac metus fermentum egestas.",
+    },
+    {
+        id: 3,
+        title: "Ciemna materia i ciemna energia",
+        author: "Ania Kwiatkowska",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia neque at risus tempus, ac condimentum nulla facilisis. Nunc sed consectetur lorem. Vivamus condimentum at lacus et malesuada. Integer luctus lacus vitae quam varius, quis imperdiet neque cursus. Pellentesque eros dui, volutpat non commodo non, pulvinar ut arcu. Integer id dictum neque, nec ultrices lectus. Phasellus scelerisque ante ac metus fermentum egestas.",
+    },
+]
 
 const HomePage = () => {
+
+    const artList = articles.map(article => (
+        <Article key={article.id} {...article}/>
+    ))
+
     return (
-        <div>Strona Startowa</div>
+        <div className="home">
+            {artList}
+        </div>
     );
 }
 
