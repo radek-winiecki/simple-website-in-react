cecfde7f5b0ac4b9293d4f1ff5c021fbceeecb09
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-23 21:10:04 +0200

    Add new images to the project and add CSS styles for them

--- project/src/layouts/Header.js
+++ project/src/layouts/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
+import "../styles/Header.css";
+
+import img1 from '../images/header1.jpg'
+import img2 from '../images/header2.jpg'
+import img3 from '../images/header3.jpg'
+import img4 from '../images/header4.jpg'
+import img5 from '../images/header5.jpg'
+
 
 const Header = () => {
     return (
-      <div>Header</div>
+        <img src={img1} alt="city"/>
     );
 }
 
