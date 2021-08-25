935f0aa3cec60f94e5a694a83776c30eb8aebd53
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-25 12:40:16 +0200

    Update the Footer and display the location of the relevant page

--- project/src/layouts/Footer.js
+++ project/src/layouts/Footer.js
@@ -1,8 +1,36 @@
 import React from "react";
+import {Route} from "react-router-dom";
+import "../styles/Footer.css"
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <h2>Footer</h2>
+            <Route path="/" render={(props) => (
+                <p>Jesteś na <span>stronie głównej</span></p>
+            )
+            }/>
+
+            <Route path="/:page" exact render={(props) => {
+                return (
+                    <>
+                        <p>Jesteś na <span>{props.match.params.page}</span></p>
+                        <p>Jesteś na <span>{props.match.url}</span></p>
+                        <p>Jesteś na <span>{props.match.path}</span></p>
+                    </>
+                )
+            }}/>
+
+            <Route path="/:page/idProduct" exact render={(props) => {
+                return (
+                    <>
+                        <p>Jesteś na <span>{props.match.params.idProduct}</span></p>
+                        <p>Jesteś na <span>{props.match.url}</span></p>
+                        <p>Jesteś na <span>{props.match.path}</span></p>
+                    </>
+                )
+            }}/>
+        </div>
     );
 }
 
